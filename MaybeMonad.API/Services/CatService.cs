9c6c044d3475090e6ca4fdd7e555905468fd1f37
Stephanie Hudson <stephanie.hudson@autotrader.co.uk>
2022-08-07 23:28:27 +0100

    initial commit

---
+++ MaybeMonad.API/Services/CatService.cs
@@ -0,0 +1,27 @@
+﻿namespace MaybeMonad.API.Services;
+
+public class CatService
+{
+    private List<Cat> cats()
+    {
+        return new List<Cat> { new(1, "Bumble"), new(2, "Kit"), new(3, "Sky") };
+    }
+
+    public Maybe<Cat> retrieveCat(int id)
+    {
+        return cats().FirstOrDefault(x => x.Id == id);
+    }
+
+    public class Cat
+    {
+        public Cat(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public int Id { get; }
+
+        public string Name { get; }
+    }
+}
---
+++ MaybeMonad.Tests/CatServiceTests.cs
@@ -0,0 +1,44 @@
+using MaybeMonad.API.Services;
+
+namespace MaybeMonad.Tests;
+
+public class CatServiceTests
+{
+    private CatService _catService;
+    
+    [SetUp]
+    public void Setup()
+    {
+        _catService = new CatService();
+    }
+
+    [Test]
+    public void givenCatExists_whenRetrieveCatName_returnCatName()
+    {
+        //arrange
+        var expectedCatName = "Bumble";
+        var expectedCat = new CatService.Cat(1, "Bumble");
+
+        //act
+        var result = _catService.retrieveCat(1);
+
+        //assert
+        Assert.AreEqual(expectedCat, result.Value);
+        Assert.AreEqual(true, result.HasValue);
+
+
+    }
+    
+    [Test]
+    public void givenCatDoesntExist_whenRetrieveCat_returnMaybe()
+    {
+        //arrange
+
+        //act
+        var result = _catService.retrieveCat(4);
+        
+        //assert
+        Assert.AreEqual(true, result.HasNoValue);
+        Assert.NotNull(result);
+    }
+}
